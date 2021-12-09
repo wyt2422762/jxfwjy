14a6975bbed3ee376bf1dce39f5a826776694e04
wyt2422762 <724752500@qq.com>
2021-12-09 17:20:22 +0800

    更新

--- ruoyi-zfjl/src/main/java/com/ruoyi/zfjl/controller/ZfjlController.java
+++ ruoyi-zfjl/src/main/java/com/ruoyi/zfjl/controller/ZfjlController.java
@@ -1,11 +1,14 @@
 package com.ruoyi.zfjl.controller;
 
-import java.net.URLEncoder;
-import java.util.List;
-
+import com.ruoyi.common.annotation.Log;
 import com.ruoyi.common.config.RuoYiConfig;
 import com.ruoyi.common.config.ServerConfig;
+import com.ruoyi.common.core.controller.BaseController;
+import com.ruoyi.common.core.domain.AjaxResult;
+import com.ruoyi.common.core.page.TableDataInfo;
+import com.ruoyi.common.enums.BusinessType;
 import com.ruoyi.common.utils.file.FileUploadUtils;
+import com.ruoyi.common.utils.poi.ExcelUtil;
 import com.ruoyi.common.validation.CreateGroup;
 import com.ruoyi.common.validation.EditGroup;
 import com.ruoyi.zfjl.domain.Zfjl;
@@ -17,17 +20,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import com.ruoyi.common.annotation.Log;
-import com.ruoyi.common.enums.BusinessType;
-import com.ruoyi.common.core.controller.BaseController;
-import com.ruoyi.common.core.domain.AjaxResult;
-import com.ruoyi.common.utils.poi.ExcelUtil;
-import com.ruoyi.common.core.page.TableDataInfo;
 import org.springframework.web.multipart.MultipartFile;
-import sun.misc.BASE64Encoder;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 /**
  * 执法记录主Controller
@@ -80,7 +75,7 @@ public class ZfjlController extends BaseController
     public AjaxResult export(Zfjl zfjl)
     {
         List<Zfjl> list = zfjlService.selectZfjlList(zfjl);
-        ExcelUtil<Zfjl> util = new ExcelUtil<Zfjl>(Zfjl.class);
+        ExcelUtil<Zfjl> util = new ExcelUtil<>(Zfjl.class);
         return util.exportExcel(list, "执法记录主数据");
     }
 
